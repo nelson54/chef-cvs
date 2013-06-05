b6880f8a1ae8e749cdb933cd1c69ac01d80d3973
Derek Nelson <derek.nelson@learninga-z.com>
2013-06-05 00:23:50 -0400

    add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name              "chef-cvs"
+maintainer        "Derek Nelson"
+maintainer_email  "derek.nelson@learninga-z.com"
+license           "Apache 2.0"
+description       "Installs CVS"
+version           "0.0.1
+
+recipe            "default", "Install CVS"
